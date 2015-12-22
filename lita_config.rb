c8c453b14f9772c0e3914ff3fa5f8488f3d54fb3
Hugo Tamaki <hugokt2005@yahoo.com.br>
2015-12-22 20:33:12 -0200

    taking out keepalive

--- lita_config.rb
+++ lita_config.rb
@@ -32,8 +32,8 @@ Lita.configure do |config|
   config.redis[:url] = ENV["REDISTOGO_URL"]
   config.http.port = ENV["PORT"]
 
-  config.handlers.keepalive.url = ENV["KEEPALIVE_URL"]
-  config.handlers.keepalive.minutes = 5
+  # config.handlers.keepalive.url = ENV["KEEPALIVE_URL"]
+  # config.handlers.keepalive.minutes = 5
 
   ## Example: Set options for the chosen adapter.
   # config.adapter.username = "myname"
